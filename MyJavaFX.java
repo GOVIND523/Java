fea693a05e10f2f6ac4e237572be0abd5a7b9c50
gopu <rajpurohitgovindsingh523@gmail.com>
2022-05-26 11:50:31 +0530

    commit

---
+++ MyJavaFX.java
@@ -0,0 +1,29 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MyJavaFX extends Application
+{
+ //@Override//
+ public void start(Stage primaryStage)
+ {
+  Scene scene = new Scene(new Button("OK") , 500 , 500);
+  primaryStage.setTitle("MyJavaFX");
+  primaryStage.setScene(scene);
+  primaryStage.show();
+
+  Stage stage = new Stage();
+  stage.setTitle("Second Stage");
+  stage.setScene(new Scene(new Button("New Stage"), 500 , 500));
+  stage.setResizable(false);
+  stage.show();
+ }
+}
+
+/*
+public static void main(String[] args)
+{
+ Application.launch(args);
+}
+*/
