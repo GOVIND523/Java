6895547fb08f9a67548a9a10ca52dc47c25c68b9
gopu <rajpurohitgovindsingh523@gmail.com>
2022-05-27 19:08:19 +0530

    Day 1

---
+++ javaTech.java
@@ -0,0 +1,68 @@
+/* 
+$ Java Technology
+
+          # Java is both a programming language and a platform.
+
+          # Java is a programming language that is simple , object oriented ,
+             distributed , multithreaded , Dynamic , Architecture independent ,
+             portable , high performance , robust , secure.
+
+          # In Java programming language, all source code is first written in
+             a plain text file with .java extension. Compiled into .class file
+             by javac compiler. .class file contains the bytecode ( Machine 
+             language of the JVM ). The java launcher tool then runs your 
+             application with an instance of Java Virtual Machine.
+
+          # The Java Platform - A platform is the hardware or software environment
+             in which a program runs. Java Platform has two components 
+             [i]  The Java Virtual Machine ( JVM )
+             [ii] The Java Application Interface ( API )
+
+          # The API is a large collection of raedy-made software components that
+             provide many useful capibilities. It is grouped into libraries of 
+             related classes and interfaces, these libraries are known as packages.
+
+$ Java capabilities
+
+          # Development tools that provide compilation , running , monitoring , 
+             debugging and documentation. jhavadoc is the documentation tool.
+          
+          # Application Programming Interface provides core functionality offering wide 
+             array of classes.
+
+          # Deployment Technologies comes with JDK to provide Java Web Software and 
+             Java Plug-In software for deploying application to end users.
+
+          # User Interface Toolkits like JavaFX , Swing and Java 2D for craeting 
+             GUI's.
+          
+          # Integration libraries enables data access and manipulation of remote
+             objects.
+
+$ Features of Java
+          # Easy to learn
+          # Write better code
+          # Write less code than C++
+          # Develop programs more quickly
+          # Avoid platform dependencies
+          # Write once , run anywhere
+          # Distribute software more easily
+
+$ Hello Universe Application
+
+$ Comments in Java 
+          # /*   ....     Multiline Comments
+          # /**  ....       Documentation 
+          # //   ....         Singleline comment
+
+*/
+
+
+
+class helloUniverse
+{
+          public static void main( String args[] )          // main method
+          {
+                    System.out.print( "Hello Universe" );   // printing to output
+          }
+}
