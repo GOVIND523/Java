7a4674cc65d3682b395b5975042f21e502bc7be5
gopu <rajpurohitgovindsingh523@gmail.com>
2022-05-28 11:37:52 +0530

    28-5-2022 Commit

--- oopJava.java
+++ oopJava.java
@@ -1,3 +1,8 @@
 /*
 $ Object 
-          #
+          # Real world objects share two characteristics they have state and 
+             behaviour.
+          
+          # Software objects are conceptually similar to real world objects.
+             The object stores the states in fields and its behaviour in 
+             methods.
